5f46ea4aad2c07fb0cb217a590fd82071efa356d
Anderson Lucas <andinlucas8@gmail.com>
2023-05-01 18:50:50 -0300

    terminando o add, update e delete

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,84 @@
-import './App.css';
+import './App.css'
 import React, { useState } from 'react';
 
 function App() {
-    // formulario
-    const [Atividade, setAtividade] = useState('')
+  const [Atividades, setAtividades] = useState([]);
+  const [novaAtividade, setnovaAtividade] = useState('');
 
-    const handleChange = (e) => {
-      // não deixa acontecer
-      e.preventDefault();
+  const handleInputChange = (e) => {
+    setnovaAtividade(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    setAtividades([...Atividades, novaAtividade]);
+    setnovaAtividade('');
+  };
+
+  const handleDeleteTask = (indice) => {
+    const novaAtividades = [...Atividades];
+    novaAtividades.splice(indice, 1);
+    setAtividades(novaAtividades);
+  };
+
+  const handleEditTask = (indice, novoValor) => {
+    const novaAtividades = [...Atividades];
+    novaAtividades[indice] = novoValor;
+    setAtividades(novaAtividades);
+  };
+
+  const Task = ({ task, indice }) => {
+    const [Editando, setEditando] = useState(false);
+    const [novoValor, setnovoValor] = useState(task);
+
+    const handleEditInputChange = (e) => {
+      setnovoValor(e.target.value);
     };
 
+    const handleSaveClick = () => {
+      handleEditTask(indice, novoValor);
+      setEditando(false);
+    };
+
+    if (Editando) {
+      return (
+        <li>
+          <input type="text" value={novoValor} onChange={handleEditInputChange} />
+          <button onClick={handleSaveClick}>Salvar</button>
+        </li>
+      );
+    }
+
     return (
-      <main>
-        <div className='container'>
-          <div className='container_titulo'>
-            <h1>Task List</h1>
-          </div>
+      <li>
+        {task}
+        <button onClick={() => setEditando(true)}>Editar</button>
+        <button onClick={() => handleDeleteTask(indice)}>Excluir</button>
+      </li>
+    );
+  };
 
-           {/* formulario */}
-          <form onSubmit={handleChange} className='container_form'>
-            <input type='text' value={Atividade} onChange={(e) => setAtividade(e.target.Atividade)} placeholder='O que vamos fazer hoje? '></input>
-            <button>Add Task</button>
-          </form>
-          {/* formulario */}
-
-          {/* lista do to do */}
-          <div className='container_list'></div>
-          {/* lista do to do */}
+  return (
+    <main >
+      <div className='container'>
+        <div className='container_titulo'>
+          <h1>Task List</h1>
         </div>
-      </main>
-  )
+        <div className='container_form'>
+          <input type="text" value={novaAtividade} onChange={handleInputChange} placeholder='O que deseja fazer hoje? ' />
+          <button onClick={handleAddTask}>Adicionar</button>
+        </div>
+        <ul className='container_list'>
+          {Atividades.map((task, indice) => (
+            <Task task={task} indice={indice} key={indice} />
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
 }
+
 export default App;
+
+
+
 
