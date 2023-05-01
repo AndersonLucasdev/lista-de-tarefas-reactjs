0731818378a8ca2ca629e98eaaa5c6dfa630c6c7
Anderson Lucas <andinlucas8@gmail.com>
2023-05-01 17:44:58 -0300

    terminando a primeira parte

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
-import Hello from './componentes.js/Hello';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Hello/>
-    </div>
-  );
-}
+    // formulario
+    const [Atividade, setAtividade] = useState('')
 
+    const handleChange = (e) => {
+      // não deixa acontecer
+      e.preventDefault();
+    };
+
+    return (
+      <main>
+        <div className='container'>
+          <div className='container_titulo'>
+            <h1>Task List</h1>
+          </div>
+
+           {/* formulario */}
+          <form onSubmit={handleChange} className='container_form'>
+            <input type='text' value={Atividade} onChange={(e) => setAtividade(e.target.Atividade)} placeholder='O que vamos fazer hoje? '></input>
+            <button>Add Task</button>
+          </form>
+          {/* formulario */}
+
+          {/* lista do to do */}
+          <div className='container_list'></div>
+          {/* lista do to do */}
+        </div>
+      </main>
+  )
+}
 export default App;
+
